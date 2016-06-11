4206114119d265e46c0a4477de8288ae6af79ecb
Gowtham Saminathan <sgowtham441@gmail.com>
2016-06-11 17:28:16 +0530

    B25_APP_GUI_UPDATE

--- ws_v0.py
+++ ws_v0.py
@@ -1,8 +1,20 @@
+#====================================================================================
+# CONNECT WEBSOCKET CLINTS USING RabbitMQ ( Exchange : DIRECT )
+# 
+#====================================================================================
+#====================================================================================
+# URL FORMATE : < PUB or SUB > <16 digit group id to join> < - > < 16 digit current user id >
+# URL FORMATE EXAMPLE : PUB123456789012345-2222222222222222
+# IF PUB : User only send the message to particular user or group
+# IF SUB:  User can send and receive from group or particular user
+#====================================================================================
+
+
 import logging
 #from guppy import hpy
 #import zlib
 #hp=hpy()
-###HAI
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 handler = logging.FileHandler('serverlog_new.log')
@@ -32,7 +44,7 @@ password = 'gowtham'
 amq_ip = '127.0.0.1'
 amq_port = 5672
 amq_Vhost = '/'
-web_port = 443
+web_port = 8080
 connection = None
 channel = None
 
@@ -98,7 +110,7 @@ class rmq_conn():
         try:
             #result = channel.queue_declare(exclusive=True)
             #self.queue_name = result.method.queue
-            print channel.queue_declare(auto_delete=True,queue = self.queue_name,
+            channel.queue_declare(auto_delete=True,queue = self.queue_name,
                                        durable=False,exclusive=False,callback=self.rmq_queue_bind)
             print 'RMQ_QUEUE_DECLARE'
             logger.info('RMQ_QUEUE_DECLARE')
@@ -109,7 +121,7 @@ class rmq_conn():
     def rmq_queue_bind(self, frame):
         global channel
         try:
-            print channel.queue_bind(exchange='SCREENSHARE',
+            channel.queue_bind(exchange='SCREENSHARE',
                                     queue=self.queue_name,
                                     routing_key=self.rmq_route,
                                     callback=self.rmq_queue_bound)
@@ -122,15 +134,15 @@ class rmq_conn():
     def rmq_queue_bound(self, frame):
         global channel
         try:
-            if self.client_type != 'PUB':
-                # CLIENT IS SUB
+            if self.client_type == 'SUB':
+                # CLIENT ACT AS PUBLISHER AND SUBSCRIBER
                 channel.basic_consume(consumer_callback=self.data_to_ws,
                                            queue=self.queue_name,
                                            no_ack=True)
                 print 'SUB CLIENT'
                 logger.info('SUB CLIENT')
-            else:
-                # CLIENT IS PUB
+            elif self.client_type == 'PUB':
+                # CLIENT ACT AS PUBLISHER ONLY
                 print 'PUB CLIENT'
                 logger.info('PUB CLIENT')
         except Exception as e:
@@ -138,13 +150,15 @@ class rmq_conn():
             logger.error('RMQ_QUEUE_BOND :'+str(e))   
     
     def data_to_ws(self, channel, method, header, body):
-        #Send the Cosumed message via Websocket to browser.
+        #Send the Consumed message via Websocket to browser.
         try:
             # STOP BROADCAST TO WHO SEND IT
+            
             if body[0:16] == self.rmq_client_id:
-                self.websocket.write_message(body,binary=True)
-            else:
                 return
+            else:
+                print "SENDING TO WS > ",len(body)
+                self.websocket.write_message(body,binary=True)
             #body = '' #Windows Fix
         except Exception as e:
             print 'DATA_TO_WS :'+str(e)
@@ -153,17 +167,9 @@ class rmq_conn():
         #UNICAST AND BROADCAST
         global channel
         try:
-            cnt = ws_msg[0]
-            if cnt == '0':
-                #IF FIRST CHAR IS 0 IT IS BROADCAST MESSAGE TO SAME ROUTE
-                route = self.rmq_route
-                ws_msg = ws_msg[1:]
-            elif cnt == '1':
-                # IF FIRST CHAR NOT 0 THEN USE 2-17 as unicast route key ( 16 chare route_key) 
-                route = ws_msg[1:17]
-                ws_msg =  ws_msg[17:] 
-            else:
-                return None
+            route = ws_msg[0:16]
+            ws_msg =  ws_msg[16:]
+            ws_msg = self.rmq_client_id+ws_msg
             #Publish the message from Websocket to RabbitMQ
             channel.basic_publish(exchange='SCREENSHARE',
                                        routing_key=route,
@@ -177,12 +183,13 @@ class rmq_conn():
         try:
             channel.queue_delete(queue=self.queue_name)
         except Exception as e:
-            print e
+            #print e
             pass
        
     #def on_closed(self,connection,close_code,close_type):
         # CLOSE TORANDO SERVER
         #tornado.ioloop.IOLoop.instance().stop()
+
 class torando_websoc(tornado.websocket.WebSocketHandler):
     def open(self,ws_url):
         try:
@@ -192,10 +199,10 @@ class torando_websoc(tornado.websocket.WebSocketHandler):
             self.rmq_conn = rmq_conn()
             self.rmq_conn.websocket = self
             rmq_client_id = ws_url.split('-')
-            if len(rmq_client_id) == 2:
-                self.rmq_conn.rmq_client_id = rmq_client_id[1]
+            if len(rmq_client_id) == 2 and len(rmq_client_id[0]) == 19 and len(rmq_client_id[1]) == 16:
+                self.rmq_conn.rmq_client_id = str(rmq_client_id[1])
                 rmq_route = rmq_client_id[0]
-                self.rmq_conn.rmq_route = rmq_route[3:]
+                self.rmq_conn.rmq_route = str(rmq_route[3:])
                 self.rmq_conn.client_type = rmq_route[0:3]
                 self.rmq_conn.queue_name = self.rmq_conn.rmq_route+str(random.randint(1,66666677))+'Q'
                 self.id2 = ioloop.add_timeout(0, self.rmq_conn.rmq_connect)
@@ -203,13 +210,18 @@ class torando_websoc(tornado.websocket.WebSocketHandler):
             else:
                 print 'INVALID URL ( CLOSING CONNECTION ):'+ws_url
                 logger.error('INVALID URL ( CLOSING CONNECTION ):'+ws_url)
+                self.close()
                 return
         except Exception as e:
             print 'WS_ON_CONNECT :'+str(e)
             logger.error('WS_ON_CONNECT :'+str(e))
+            self.close()
+            return
+        
     
     def on_message(self,msg):
         try:
+            print "RECEIVED FROM WS>",len(msg)
             self.rmq_conn.data_to_rmq(msg)
             #print hp.heap().byrcs[0].byid
         except Exception as e:
